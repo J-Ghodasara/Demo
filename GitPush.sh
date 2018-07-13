37d2b600acb6c0e43a3580bda697a1d48ed9ff34
J-Ghodasara <jay.ghodasara.sa@gmail.com>
2018-07-13 21:08:26 +0530

    Auto generated Commit

--- GitPush.sh
+++ GitPush.sh
@@ -4,6 +4,8 @@ git add *
 git commit -m "Auto generated Commit"
 git config --global user.email "jay.ghodasara.sa@gmail.com"
 git config --global user.name "J-Ghodasara"
+git remote add origin $1
+git push --force origin master
 
 
 
